e4d6d2666975ebd60e4ffd2565535a18d93baf2a
Gabriel Veras <gabrielverasm@gmail.com>
2026-06-24 10:12:42 -0300

    feat: configure oidc provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,28 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { LogLevel, provideAuth } from 'angular-auth-oidc-client';
 
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withInMemoryScrolling({ anchorScrolling: 'enabled' })),
     provideHttpClient(),
+    provideAuth({
+      config: {
+        authority: environment.oidc.issuer,
+        redirectUrl: environment.oidc.redirectUri,
+        postLogoutRedirectUri: environment.oidc.postLogoutRedirectUri,
+        clientId: environment.oidc.clientId,
+        scope: environment.oidc.scope,
+        responseType: 'code',
+        silentRenew: true,
+        useRefreshToken: true,
+        logLevel: environment.production ? LogLevel.None : LogLevel.Debug,
+      },
+    }),
   ],
 };
