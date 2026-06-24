db0723210191ac413a923e8bf06854c9a8d08688
Gabriel Veras <gabrielverasm@gmail.com>
2026-06-24 10:19:58 -0300

    feat: initialize auth session

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { take } from 'rxjs';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,10 @@ import { RouterOutlet } from '@angular/router';
   template: '<router-outlet />',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private readonly authService = inject(AuthService);
+
+  ngOnInit(): void {
+    this.authService.checkAuth().pipe(take(1)).subscribe();
+  }
+}
