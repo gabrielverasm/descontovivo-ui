db05532f6b16dc696db27bfb7ada1159216e4e63
Gabriel Veras <gabrielverasm@gmail.com>
2026-06-24 10:29:34 -0300

    feat: add email verified guard

---
+++ src/app/core/guards/email-verified.guard.ts
@@ -0,0 +1,19 @@
+import { inject } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
+import { map, take } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+export const emailVerifiedGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+
+  return authService.checkAuth().pipe(
+    take(1),
+    map((isAuthenticated) => {
+      if (!isAuthenticated) {
+        authService.login();
+        return false;
+      }
+      return authService.canPublish();
+    }),
+  );
+};
